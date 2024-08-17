9e5d6d8fecd76cf026c86b93da8aa0828671e764
Nikhilesh Gurjar <nikhilesh.gurjar@openxcell.com>
2024-08-17 09:54:18 +0530

    File name updated

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,4 @@
-const User = require('../models/user.js');
+const User = require('../models/User.js');
 const jwt = require('jsonwebtoken');
 const { JWT_SECRET } = require('../config/default.json');
 
